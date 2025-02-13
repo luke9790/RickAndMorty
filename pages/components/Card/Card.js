7d430ba430fd8ff9eceef8370be5848db97f5d00
lmasetti <luca9790@gmail.com>
2025-02-13 14:21:30 +0100

    fix card

--- pages/components/Card/Card.js
+++ pages/components/Card/Card.js
@@ -1,4 +1,9 @@
 function Card({ character }) {
+  if (!character) {
+    // oppure puoi renderizzare un placeholder o un messaggio di errore
+    return <div>Character data not available</div>;
+  }
+  
   const { name, image, status, species, location } = character;
   const statusColor = {
     alive: "text-green-500",
@@ -16,15 +21,11 @@ function Card({ character }) {
       />
       <div className="p-4 bg-blue-gray-800 text-white flex-grow">
         <h3 className="text-lg font-bold">{name}</h3>
-        <p
-          className={`${
-            statusColor[status.toLowerCase()]
-          } flex items-center mt-2`}
-        >
+        <p className={`${statusColor[status.toLowerCase()]} flex items-center mt-2`}>
           <span className="h-3 w-3 rounded-full bg-current inline-block mr-2"></span>
           {status} - {species}
         </p>
-        <p className="text-sm mt-1">Last Location: {location.name}</p>
+        <p className="text-sm mt-1">Last Location: {location?.name || "Unknown"}</p>
       </div>
     </div>
   );
